609725b9f78976c24b1fd18cf9fa5de575b84a06
Martin Murillo <jmmurilloj@hotmail.com>
2022-04-10 16:32:00 -0500

    tracking added

--- js/tracking-splitbee.js
+++ js/tracking-splitbee.js
@@ -1,4 +1,8 @@
+
+jQuery(document).ready(function($){
+splitbee.init()
 splitbee.track("dataseten22-d")
 splitbee.track("dataseten22-f")
 splitbee.track("dataseten22-l")
 splitbee.track("dataseten22-t")
+});
